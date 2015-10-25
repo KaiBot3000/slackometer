87fc003e120e2f9256cde6fb532b5642f0957b66
Kai <riselikethemoon@gmail.com>
2015-10-25 14:10:33 -0700

    redirecting to slack for OAuth

--- server.py
+++ server.py
@@ -1,2 +1,34 @@
-import sys
+import os
+from flask import Flask, render_template, redirect, request
+from urllib import urlencode
 
+app = Flask(__name__)
+
+app.secret_key = "a_very_dumb_secret"
+
+client_id = os.environ["CLIENT_ID"]
+client_secret = os.environ["CLIENT_SECRET"]
+
+@app.route("/")
+def index_page():
+    """Initial landing page"""
+
+    params = {"client_id": client_id, "redirect_uri": "http://localhost:5000/slacked"}
+
+    oauth_url = "https://slack.com/oauth/authorize?" + urlencode(params)
+
+    return redirect(oauth_url)
+
+
+@app.route("/slacked")
+def slacked():
+    """Landing page for authorized slack users"""
+
+
+
+    return effort
+
+
+if __name__ == '__main__':
+    print "Server up and running, yo!"
+    app.run()
