3694c880641d9cfbb4844590ebf1b0df7e4bdc43
Kai <riselikethemoon@gmail.com>
2015-10-25 14:48:25 -0700

    added random string function for state

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-import os
+import os, random, string
 from flask import Flask, render_template, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
 from urllib import urlencode
+
 
 app = Flask(__name__)
 
@@ -13,7 +15,11 @@ client_secret = os.environ["CLIENT_SECRET"]
 def index_page():
     """Initial landing page"""
 
-    params = {"client_id": client_id, "redirect_uri": "http://localhost:5000/slacked"}
+
+
+    params = {"client_id": client_id,
+                "redirect_uri": "http://localhost:5000/slacked",
+                "state": randomWord()}
 
     oauth_url = "https://slack.com/oauth/authorize?" + urlencode(params)
 
@@ -24,9 +30,16 @@ def index_page():
 def slacked():
     """Landing page for authorized slack users"""
 
-
+    effort = "Successful!"
 
     return effort
+
+
+# Helper functions
+
+def randomWord():
+    """Generates a random string, to use as an API verification"""
+    return ''.join(random.choice(string.lowercase) for i in range(10))
 
 
 if __name__ == '__main__':
