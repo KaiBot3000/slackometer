8d153155d1b58eab3538c56adfb2c6a4ec5f0b3e
Kai <riselikethemoon@gmail.com>
2015-11-04 11:06:20 -0800

    moved over class methods

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, request
 from flask_debugtoolbar import DebugToolbarExtension
 from urllib import urlencode, urlopen
 import re
+import channel
 
 
 app = Flask(__name__)
@@ -107,91 +108,91 @@ def get_channel_list(token):
     return channel_list
 
 
-def get_channel_history(token, channel_tuple):
-    """Given a channel tuple, returns history of the channel"""
+# def get_channel_history(token, channel_tuple):
+#     """Given a channel tuple, returns history of the channel"""
 
-    history_params = {"token": token, 
-                        "channel": channel_tuple[1],
-                        "inclusive": 1,
-                        "count":30
-                        }
-    history_url = "https://slack.com/api/channels.history?" + urlencode(history_params)
-    json_history = requests.get(history_url)
-    history_response = json_history.json()
+#     history_params = {"token": token, 
+#                         "channel": channel_tuple[1],
+#                         "inclusive": 1,
+#                         "count":30
+#                         }
+#     history_url = "https://slack.com/api/channels.history?" + urlencode(history_params)
+#     json_history = requests.get(history_url)
+#     history_response = json_history.json()
 
-    msg_list = []
+#     msg_list = []
 
-    for msg in history_response["messages"]:
-        msg_list.append(msg["text"])
+#     for msg in history_response["messages"]:
+#         msg_list.append(msg["text"])
 
-    return msg_list
-
-
-def make_history_dictionary(msg_list):
-    """Converts a message list into a dictionary for sentiment analysis"""
-
-    # dictionary = {"data":[
-    #                     {"text": "I love Titanic."}, 
-    #                     {"text": "I hate Titanic."}
-    #                     ]}
-
-    msg_dictionary = {}
-    msg_text_list = []
-
-    for msg in msg_list:
-        msg_dict = {}
-        msg = clean_msg(msg)
-        msg_dict["text"] = msg
-        msg_text_list.append(msg_dict)
-
-    msg_dictionary["data"] = msg_text_list
-
-    return msg_dictionary
+#     return msg_list
 
 
-def clean_msg(msg):
-    """Takes single message, removes user tags and links, returns stripped message"""
+# def make_history_dictionary(msg_list):
+#     """Converts a message list into a dictionary for sentiment analysis"""
 
-    # things to remove: <usernames> <links...>
+#     # dictionary = {"data":[
+#     #                     {"text": "I love Titanic."}, 
+#     #                     {"text": "I hate Titanic."}
+#     #                     ]}
 
-    cleaned_msg = re.sub("[<].*?[>]", "", msg)
+#     msg_dictionary = {}
+#     msg_text_list = []
 
-    return cleaned_msg
+#     for msg in msg_list:
+#         msg_dict = {}
+#         msg = clean_msg(msg)
+#         msg_dict["text"] = msg
+#         msg_text_list.append(msg_dict)
+
+#     msg_dictionary["data"] = msg_text_list
+
+#     return msg_dictionary
 
 
-def get_sentiment(msg_dictionary):
-    """Given a message dictionary, makes an API call to Sentiment140 to get sentiments"""
+# def clean_msg(msg):
+#     """Takes single message, removes user tags and links, returns stripped message"""
 
-    msg_dictionary["appid"] = MYEMAIL
-    sentiment_data = json.dumps(msg_dictionary)
+#     # things to remove: <usernames> <links...>
 
-    sentiment_api_call = urlopen('http://www.sentiment140.com/api/bulkClassifyJson', sentiment_data)
-    sentiment_response = sentiment_api_call.read()
+#     cleaned_msg = re.sub("[<].*?[>]", "", msg)
+
+#     return cleaned_msg
+
+
+# def get_sentiment(msg_dictionary):
+#     """Given a message dictionary, makes an API call to Sentiment140 to get sentiments"""
+
+#     msg_dictionary["appid"] = MYEMAIL
+#     sentiment_data = json.dumps(msg_dictionary)
+
+#     sentiment_api_call = urlopen('http://www.sentiment140.com/api/bulkClassifyJson', sentiment_data)
+#     sentiment_response = sentiment_api_call.read()
      
-    return sentiment_response
+#     return sentiment_response
 
-def make_sentiment_list(sentiment_dict):
-    """Given the sentiment response dictionary, makes list of just the sentiment values"""
+# def make_sentiment_list(sentiment_dict):
+#     """Given the sentiment response dictionary, makes list of just the sentiment values"""
 
-    sentiment_list = []
+#     sentiment_list = []
 
-    for sentiment in sentiment_dict["data"]:
-        sentiment_list.append(sentiment["polarity"])
+#     for sentiment in sentiment_dict["data"]:
+#         sentiment_list.append(sentiment["polarity"])
 
-    return sentiment_list
+#     return sentiment_list
 
 
-new_response = {"data":[
-                    {"text":" has joined the channel","polarity":2,"meta":{"language":"en"}},
-                    {"text":"awww!","polarity":2,"meta":{"language":"en"}},
-                    {"text":"Thank you!  I was pretty proud of myself.","polarity":2,"meta":{"language":"en"}},
-                    {"text":"Awwwww","polarity":2,"meta":{"language":"en"}},
-                    {"text":"is that a stencil ? awesome!","polarity":4,"meta":{"language":"en"}}
-                    ],
-                "appid":"kai@kaidalgleish.io"}
+# new_response = {"data":[
+#                     {"text":" has joined the channel","polarity":2,"meta":{"language":"en"}},
+#                     {"text":"awww!","polarity":2,"meta":{"language":"en"}},
+#                     {"text":"Thank you!  I was pretty proud of myself.","polarity":2,"meta":{"language":"en"}},
+#                     {"text":"Awwwww","polarity":2,"meta":{"language":"en"}},
+#                     {"text":"is that a stencil ? awesome!","polarity":4,"meta":{"language":"en"}}
+#                     ],
+#                 "appid":"kai@kaidalgleish.io"}
 
-new_list = make_sentiment_list(new_response)
-print new_list
+# new_list = make_sentiment_list(new_response)
+# print new_list
 
 
 if __name__ == '__main__':
